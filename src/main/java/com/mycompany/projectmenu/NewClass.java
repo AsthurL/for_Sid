3432e6c0d9454812bb2b8568c37cfe18fec37466
Asthur Araujo <Asthur Araujo@DESKTOP-S6AN4DA>
2021-02-23 00:49:40 -0300

    test commit

---
+++ src/main/java/com/mycompany/projectmenu/NewClass.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.projectmenu;
+
+/**
+ *
+ * @author Asthur Araujo
+ */
+public class NewClass {
+public static void main (String[] args){
+    System.out.println("Hello gitHub");
+  
+    
+    
+    
+    
+}}
