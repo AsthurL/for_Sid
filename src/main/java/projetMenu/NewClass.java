290d7c896cad1a180d708e24019eac2958aea2ed
Asthur Araujo <Asthur Araujo@DESKTOP-S6AN4DA>
2021-02-24 23:37:23 -0300

    Projeto menu finalizado

--- src/main/java/projetMenu/NewClass.java
+++ src/main/java/projetMenu/NewClass.java
@@ -1,7 +1,9 @@
-
 package projetMenu;
+
 import projetMenu.studentRecurse;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,132 +12,170 @@ import java.util.Scanner;
  * @author Asthur Araujo
  */
 public class NewClass {
-public static void main (String[] args){
-    Scanner sc = new Scanner(System.in);
-    Locale.setDefault(Locale.US);
-        
-    
-    
-    ArrayList<studentRecurse> student = new ArrayList<>();
-    Integer pick = 0;
-    
-    
-    
-    
-    
-    
-    
-    
-    for(int y = 5; pick <y;){
-    System.out.println("*MENU*"+"\n"+"\n"+"1: INCLUIR ALUNO"+"\n"+ "2: ALTERAR ALUNO"+"\n"+ "3: IMPRIMIR ALUNO"+"\n"+ "4: IMPRIMIR TODOS OS ALUNOS"+"\n"+"5: FECHAR PROGRAMA");
-    pick = sc.nextInt();
-     while (pick < 1 || pick > 5) {
-				System.out.println("Valor invalido, DIGITE NOVAMETE: ");
-				pick = sc.nextInt();}
 
-    
-    
-    
-    
-    
-    switch(pick){
-        
-    case 1: 
-    System.out.println("#INSIRA OS DADOS DO ALUNO:");
-			System.out.println("ID do aluno:");
-			int id = sc.nextInt();
-			
-                        Integer idConference = studentRecurse.position(student, id);
-                        while(idConference != null){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        ArrayList<studentRecurse> student = new ArrayList<>();
+        Integer pick = 0;
+
+        for (int y = 5; pick < y;) {
+            System.out.println("\n" + "*MENU*" + "\n" + "1: INCLUIR ALUNO" + "\n" + "2: ALTERAR ALUNO" + "\n" + "3: IMPRIMIR ALUNO" + "\n" + "4: IMPRIMIR TODOS OS ALUNOS" + "\n" + "5: FECHAR PROGRAMA");
+            pick = sc.nextInt();
+            while (pick < 1 || pick > 5) {
+                System.out.println("Valor invalido, DIGITE NOVAMETE: ");
+                pick = sc.nextInt();}
+           
+            switch (pick) {
+                case 1:
+                    System.out.println("#INSIRA OS DADOS DO ALUNO:");
+                    System.out.println("ID do aluno:");
+                    int id = sc.nextInt();
+                    Integer idConference = studentRecurse.position(student, id);
+                    while (idConference != null) {
                         System.out.print("ID Ja cadastrado! DIGITE NOVAMENTE: ");
                         System.out.println("");
                         id = sc.nextInt();
                         idConference = studentRecurse.position(student, id);}
-			System.out.println("Nome do aluno:");
-			sc.nextLine();
-                        String name = sc.nextLine();
-			System.out.println("Curso do aluno:");
-			String course = sc.nextLine();
-			System.out.println("Nota do primeiro semestre:");
-			Double semester1 = sc.nextDouble();
-                        
-			while (semester1 < 0 || semester1 > 10) {
-				System.out.println("Noda do primeiro semestre não econtra-se entre 0 e 10! DIGITE NOVAMENTE: ");
-				semester1 = sc.nextDouble();}
-                      
-			System.out.println("Nota do segundo semestre:");
-			Double semester2 = sc.nextDouble();
-			while (semester2 < 0 || semester2 > 10) {
-				System.out.println("Noda do segundo semestre não econtra-se entre 0 e 10! DIGITE NOVAMENTE: ");
-				semester2 = sc.nextDouble();}
-                      
-                        studentRecurse stud = new studentRecurse(id, name, course, semester1, semester2);
-			student.add(stud);
-                                System.out.println("DADOS:"+ "\n"+stud);
-    break;
-    
-    case 2: 
-    System.out.println("Insira o ID do aluno:");    
-        id = sc.nextInt();
-        idConference = studentRecurse.position(student, id);
-        while(idConference == null){
-                        System.out.print("ID não cadastrado! DIGITE NOVAMENTE: ");
-                        System.out.println("");
+                    System.out.println("Nome do aluno:");
+                    sc.nextLine();
+                    String name = sc.nextLine();
+                    System.out.println("Curso do aluno:");
+                    String course = sc.nextLine();
+                    System.out.println("Nota do primeiro semestre:");
+                    Double semester1 = sc.nextDouble();
+                    
+                    while (semester1 < 0 || semester1 > 10) {
+                        System.out.println("Noda do primeiro semestre não econtra-se entre 0 e 10! DIGITE NOVAMENTE: ");
+                        semester1 = sc.nextDouble();}
+                    System.out.println("Nota do segundo semestre:");
+                    Double semester2 = sc.nextDouble();
+                   
+                    while (semester2 < 0 || semester2 > 10) {
+                        System.out.println("Noda do segundo semestre não econtra-se entre 0 e 10! DIGITE NOVAMENTE: ");
+                        semester2 = sc.nextDouble();}                    
+                    student.add( new studentRecurse(id, name, course, semester1, semester2));
+                    System.out.println("DADOS:" + "\n" + student + "\n");
+                    break;
+
+
+                case 2:
+                    if (student.size() != 0) {
+                        System.out.println("Insira o ID do aluno:");
                         id = sc.nextInt();
-                        idConference = studentRecurse.position(student, id);}
-        System.out.println("Qual dado deseja alterar?:");
-        System.out.println();
-        System.out.println("1: ID DO ALUNO"+"\n"+"2: NOME DO ALUNO"+"\n"+ "3: CURSO DO ALUNO"+"\n"+ "4: NOTAS"+"\n"+ "5: RETORNAR");
-       pick = sc.nextInt();
-       sc.nextLine();
-        switch(pick){
-            case 1: System.out.print("NOVO ID: ");
-          System.out.println("");
-            student.get(idConference).setId(sc.nextInt());
-                break;
-                case 2: System.out.print("NOVO NOME: ");
-                System.out.println("");
-            student.get(idConference).setName(sc.nextLine());
-            //sc.nextLine();
-                break;
-                case 3: System.out.print("NOVO CURSO: ");
-                System.out.println("");
-            student.get(idConference).setCourse(sc.nextLine());
-            //sc.nextLine();
-                break;
-                case 4: System.out.print("NOTA PRIMEIRO SEMESTRE: ");
-                System.out.println("");
-            student.get(idConference).setSemester1(sc.nextDouble());
-         System.out.print("NOTA SEGUNDO SEMESTRE: ");
-         System.out.println("");
-            student.get(idConference).setSemester2(sc.nextDouble());
-            student.get(idConference).setHighNote(studentRecurse.highNote(student.get(idConference).getSemester1(), student.get(idConference).getSemester2()));
-          student.get(idConference).setHighNote(studentRecurse.average(student.get(idConference).getSemester1(), student.get(idConference).getSemester2()));
-            break;
-        }
-        
-    break;
-    
-    case 3: 
-    break;
-    
-    case 4: 
-        for ( studentRecurse a : student)
-        {System.out.print(a);}
-    break;
-   default:
-        while(0 >= pick && 5 <= pick){System.out.println("Valor informado incorreto! DIGITE NOVAMENTE: "); pick = sc.nextInt();}
-    }
-    
-    
-    
-    
-    
-}
+                        idConference = studentRecurse.position(student, id);
+                        while (idConference == null) {
+                            System.out.print("ID não cadastrado! DIGITE NOVAMENTE: ");
+                            System.out.println("");
+                            id = sc.nextInt();
+                            idConference = studentRecurse.position(student, id);}
+                        
+                        System.out.println("Qual dado deseja alterar?:");
+                        System.out.println();
+                        System.out.println("1: ID DO ALUNO" + "\n" + "2: NOME DO ALUNO" + "\n" + "3: CURSO DO ALUNO" + "\n" + "4: NOTAS" + "\n" + "5: RETORNAR");
+                        pick = sc.nextInt();
+                        sc.nextLine();
+                        switch (pick) {
+                            case 1:
+                                System.out.print("NOVO ID: ");
+                                System.out.println("");
+                                student.get(idConference).setId(sc.nextInt());
+                                break;
+                            case 2:
+                                System.out.print("NOVO NOME: ");
+                                System.out.println("");
+                                student.get(idConference).setName(sc.nextLine());
+                                while (student.get(idConference).getName().trim().equalsIgnoreCase("")) {
+                                    System.out.print("Nome invalido! DIGITE NOVAMENTE: ");
+                                    System.out.println("");
+                                    student.get(idConference).setName(sc.nextLine());}                                
+                                break;
+                            case 3:
+                                System.out.print("NOVO CURSO: ");
+                                System.out.println("");
+                                student.get(idConference).setCourse(sc.nextLine());
+                                while (student.get(idConference).getCourse().trim().equalsIgnoreCase("")) {
+                                    System.out.print("Nome invalido! DIGITE NOVAMENTE: ");
+                                    System.out.println("");
+                                    student.get(idConference).setCourse(sc.nextLine());}                               
+                                break;
+                            case 4:
+                                System.out.print("NOTA PRIMEIRO SEMESTRE: ");
+                                System.out.println("");
+                                student.get(idConference).setSemester1(sc.nextDouble());
+                                while (student.get(idConference).getSemester1() < 0 || student.get(idConference).getSemester1() > 10) {
+                                    System.out.println("Noda do primeiro semestre não econtra-se entre 0 e 10! DIGITE NOVAMENTE: ");
+                                    student.get(idConference).setSemester1(sc.nextDouble());}
+                                System.out.print("NOTA SEGUNDO SEMESTRE: ");
+                                System.out.println("");
+                                student.get(idConference).setSemester2(sc.nextDouble());
+                                while (student.get(idConference).getSemester2() < 0 || student.get(idConference).getSemester2() > 10) {
+                                    System.out.println("Noda do primeiro semestre não econtra-se entre 0 e 10! DIGITE NOVAMENTE: ");
+                                    student.get(idConference).setSemester2(sc.nextDouble());;}                               
+                                break;}}                        
+                    else {
+                        System.out.println("NENHUM ALUNO CADASTRADO");}
+                    break;
+                case 3:
+                    if (student.size() != 0) {
+                        System.out.println("Insira o ID do aluno que deseja visualizar:");
+                        id = sc.nextInt();
+                        idConference = studentRecurse.position(student, id);
+                        while (idConference == null) {
+                            System.out.print("ID não encontrado! DIGITE NOVAMENTE: ");
+                            System.out.println("");
+                            id = sc.nextInt();
+                            idConference = studentRecurse.position(student, id);}                        
+                        System.out.println("DADOS:" + "\n" + student.get(idConference));}
+                     else {
+                        System.out.println("NENHUM ALUNO CADASTRADO");}                  
+                    break;
+                case 4:
+                    System.out.println("Como deseja exibir os dados?: ");
+                    System.out.println("1 = ORDER OF INCLUSION");
+                    System.out.println("2 = NAME ORDER");
+                    System.out.println("3 = ID ORDER");
+                    System.out.println("4 = AVERAGE ORDER");
+                    int order = sc.nextInt();
+                    while (order < 1 || order > 4) {
+                        System.out.println("Valor invalido, DIGITE NOVAMETE: ");
+                        order = sc.nextInt();}                   
+                    switch (order) {
+                        case 1:
+                            for (studentRecurse a : student) {
+                                System.out.print(a);}                            
+                            break;
+                        case 2:
+                            Comparator compare = new Comparator<studentRecurse>() {
+                                public int compare(studentRecurse student, studentRecurse otherStudent) {
+                                    return student.getName().compareTo(otherStudent.getName());}};                             
+                            Collections.sort(student, compare);
+                            System.out.println("Segue dados cadastrados: ");
+                            for (studentRecurse b : student) {
+                                System.out.println(b);
+                                System.out.println("---------------------------------");}                           
+                            break;
+                        case 3:
+                            Collections.sort(student);
+                            System.out.println("Segue dados cadastrados: ");
+                            for (studentRecurse b : student) {
+                                System.out.println(b);
+                                System.out.println("---------------------------------");}                           
+                            break;
+                        case 4:
+                            Collections.sort(student, new Comparator<studentRecurse>() {
+                                public int compare(studentRecurse student, studentRecurse otherStudent) {
+                                    if (student.getAverage() < otherStudent.getAverage()) {
+                                        return 1;
+                                    } else if (student.getAverage() > otherStudent.getAverage()) {return -1;}                                   
+                                     else {return 0;}}});                 
+                            System.out.println("Segue dados cadastrados: ");
+                            for (studentRecurse b : student) {
+                                System.out.println(b);
+                                System.out.println("---------------------------------");}
+                             break;}
+                      break;}} 
+        System.out.println("PROGRAMA FECHADO");
+    }}
 
-System.out.println("PROGRAMA FECHADO");
-
-
-
-}
-}
--- src/main/java/projetMenu/studentRecurse.java
+++ src/main/java/projetMenu/studentRecurse.java
@@ -1,177 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projetMenu;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Asthur Araujo
- */
-public class studentRecurse implements Comparable<studentRecurse>{
-  
-    /*
-    private String name, course;
+public class studentRecurse implements Comparable<studentRecurse> {
+
+    private String name;
+    private String course;
     private Double semester1;
     private Double semester2;
-    
-    private Double highNote; {if(semester1 > semester2){highNote = (double) semester1;} if(semester1 < semester2){highNote = (double) semester2;};}
-
     private Integer id, order;
-    private Double average = (semester1 + semester2) / 2;
-    private Double sumAverage = (semester1 + semester2) / 2; */
-		
-    private static String name;
-    private static String course;
-    private static Double semester1;
-    private static Double semester2; 
-    private static Double highNote;
-    private static Integer id, order;
     private static Double average;
-   // private Double sumAverage = (double) 0;
+    private static Double highNote;
 
-    public void setHighNote(Double highNote) {
-        this.highNote = highNote;
-    }
-
-    public void setOrder(Integer order) {
-        this.order = order;
-    }
-
-    public void setAverage(Double average) {
-        this.average = average;
-    }
-   
-    //public static double geralAverage (ArrayList<studentRecurse> student) {
-	//	return sumAverage / student.size();}
-    
-    public static Double  average (Double semester1, Double semester2){return average = (semester1 + semester2) / 2;}
-    public static Double highNote (Double semester1, Double semester2){
-    if(semester1 > semester2){return highNote = semester1;} else{return highNote = semester2;}}
-    
-    
-    
-    
     public studentRecurse(Integer id, String name, String course, Double semester1, Double semester2) {
-		this.name = name;
-		this.course = course;
-		this.semester1 = (double) semester1;
-		this.semester2 = (double) semester2;
-		this.id = id;
-                average = this.average;
-                highNote = this.highNote;
-		//this.sumAverage += (semester1 + semester2) / 2;
-		//if(semester1 > semester2){highNote = semester1;};if(semester1 < semester2){highNote = semester2;};
-		//average = (semester1 + semester2) / 2;
-    }
-    public String toString() {
-			return "ID: "+id+"\n"+
-		           "Name: "+name+"\n"+
-		           "Course: "+course+"\n"+
-				   "Average: "+String.format("%.2f", average)+"\n"+
-			"Student high note: "+ String.format("%.2f", highNote)+
-                                "\n";} 
-    
-   
-
-    public static Integer position (ArrayList <studentRecurse>student, int id) {
-		for (int i = 0; i < student.size(); i++) {
-                    
-			if (student.get(i).getId() == id) {return i;}}return null;} //Retorna validação ID
-
-    
-    
-    
-   
-
-  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
- 
-    
-    
-    
-    
-    
-    
-    
-    
-     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setCourse(String course) {
         this.course = course;
-    }
+        this.semester1 = (double) semester1;
+        this.semester2 = (double) semester2;
+        this.id = id;}
 
+    public String toString() {
+        return "ID: " + id + "\n"
+                + "Name: " + name + "\n"
+                + "Course: " + course + "\n"
+                + "Average: " + String.format("%.2f", getAverage()) + "\n"
+                + "Student high note: " + String.format("%.2f", getHighNote())
+                + "\n";}
+    
+    public static Integer position(ArrayList<studentRecurse> student, int id) {
+        for (int i = 0; i < student.size(); i++) {if (student.get(i).getId() == id) { return i;}} return null;}  //Retorna validação ID          
+ 
+    public void setHighNote(Double highNote) {
+        this.highNote = highNote; }
+   
+    public void setOrder(Integer order) {
+        this.order = order;}
+    
+    public void setAverage(Double average) {
+        this.average = average;}
+    
+    public void setName(String name) {
+        this.name = name;}
+    
+    public void setCourse(String course) {
+        this.course = course;}
+    
     public void setSemester1(Double semester1) {
-        this.semester1 = semester1;
-    }
-
+        this.semester1 = semester1;}
+    
     public void setSemester2(Double semester2) {
-        this.semester2 = semester2;
-    }
-
+        this.semester2 = semester2;}
+    
     public void setId(Integer id) {
-        this.id = id;
-    }
-    
-    
-
+        this.id = id;}
     
     public String getName() {
-        return name;
-    }
-
+        return name;}
+    
     public String getCourse() {
-        return course;
-    }
-
+        return course;}
+    
     public Double getSemester1() {
-        return semester1;
-    }
-
+        return semester1;}
+    
     public Double getSemester2() {
-        return semester2;
-    }
+        return semester2;}
 
     public Double getHighNote() {
-        return highNote;
-    }
-
+        if (semester1 > semester2) {
+            return semester1;
+        } else { return semester2;}}
+           
     public Integer getId() {
-        return id;
-    }
-
-    public Integer getOrder() {
-        return order;
-    }
-
-    public Double getAverage() {
-        return average;
-    }
-
+        return id;}
    
-
+    public Integer getOrder() {
+        return order;}
     
+    public Double getAverage() {
+        return (semester1 + semester2) / 2;}
+
     public int compareTo(studentRecurse otherStudent) {
-        if (this.id > otherStudent.getId()) { 
-				  return 1; } if (this.id < otherStudent.getId()) {return -1;} return 0;
-    }
- 
-    
-
-    
-    
+        if (this.id > otherStudent.getId()) {
+            return 1;}       
+        if (this.id < otherStudent.getId()) {
+            return -1;}
+        return 0;}
+        
 }
